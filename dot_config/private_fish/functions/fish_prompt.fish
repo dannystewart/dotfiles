459bec09041a95952aad08d853668977559a723e
Danny Stewart <danny@stewart.cc>
2025-06-24 21:05:32 -0400

    feat(fish): implement Tide prompt and Fisher package manager
    Replaces the custom fish prompt with the Tide prompt framework for a more feature-rich shell experience. The changes include:
    
    - Installing Fisher package manager for fish shell
    - Adding Tide v6 as a prompt framework with customized configuration
    - Setting up prompt features including git status, command duration, and various language/tool indicators
    - Configuring virtual environment prompt behavior
    - Implementing transient prompt functionality for better terminal readability
    
    The new prompt provides better visual indicators for development environments and system status while maintaining a clean interface.

--- dot_config/private_fish/functions/fish_prompt.fish
+++ dot_config/private_fish/functions/fish_prompt.fish
@@ -1,90 +1,171 @@
 function fish_prompt
-    set -l last_status $status
-    set -l normal (set_color normal)
-    set -l usercolor (set_color $fish_color_user)
+end # In case this file gets loaded non-interactively, e.g by conda
+status is-interactive || exit
 
-    # Add a blank line for visual separation (except for the very first prompt)
-    if test $CMD_DURATION
-        echo
+_tide_remove_unusable_items
+_tide_cache_variables
+_tide_parent_dirs
+source (functions --details _tide_pwd)
+
+set -l prompt_var _tide_prompt_$fish_pid
+set -U $prompt_var # Set var here so if we erase $prompt_var, bg job won't set a uvar
+
+set_color normal | read -l color_normal
+status fish-path | read -l fish_path
+
+# _tide_repaint prevents us from creating a second background job
+function _tide_refresh_prompt --on-variable $prompt_var --on-variable COLUMNS
+    set -g _tide_repaint
+    commandline -f repaint
+end
+
+if contains newline $_tide_left_items # two line prompt initialization
+    test "$tide_prompt_add_newline_before" = true && set -l add_newline '\n'
+
+    set_color $tide_prompt_color_frame_and_connection -b normal | read -l prompt_and_frame_color
+
+    set -l column_offset 5
+    test "$tide_left_prompt_frame_enabled" = true &&
+        set -l top_left_frame "$prompt_and_frame_color╭─" &&
+        set -l bot_left_frame "$prompt_and_frame_color╰─" &&
+        set column_offset (math $column_offset-2)
+    test "$tide_right_prompt_frame_enabled" = true &&
+        set -l top_right_frame "$prompt_and_frame_color─╮" &&
+        set -l bot_right_frame "$prompt_and_frame_color─╯" &&
+        set column_offset (math $column_offset-2)
+
+    if test "$tide_prompt_transient_enabled" = true
+        eval "
+function fish_prompt
+    _tide_status=\$status _tide_pipestatus=\$pipestatus if not set -e _tide_repaint
+        jobs -q && jobs -p | count | read -lx _tide_jobs
+        $fish_path -c \"set _tide_pipestatus \$_tide_pipestatus
+set _tide_parent_dirs \$_tide_parent_dirs
+PATH=\$(string escape \"\$PATH\") CMD_DURATION=\$CMD_DURATION fish_bind_mode=\$fish_bind_mode set $prompt_var (_tide_2_line_prompt)\" &
+        builtin disown
+
+        command kill \$_tide_last_pid 2>/dev/null
+        set -g _tide_last_pid \$last_pid
     end
 
-    # Git prompt
-    set -g __fish_git_prompt_showdirtystate 1
-    set -g __fish_git_prompt_showuntrackedfiles 1
-    set -g __fish_git_prompt_showupstream informative
-    set -g __fish_git_prompt_showcolorhints 1
-    set -g __fish_git_prompt_use_informative_chars 1
-    string match -qi "*.utf-8" -- $LANG $LC_CTYPE $LC_ALL
-    and set -g __fish_git_prompt_char_dirtystate \U1F4a9
-    set -g __fish_git_prompt_char_untrackedfiles " ?"
-    set -g __fish_git_prompt_char_cleanstate " ✓"
+    if not set -q _tide_transient
+        math \$COLUMNS-(string length -V \"\$$prompt_var[1][1]\$$prompt_var[1][3]\")+$column_offset | read -lx dist_btwn_sides
 
-    set -l vcs (fish_vcs_prompt '%s' 2>/dev/null)
+        echo -n $add_newline'$top_left_frame'(string replace @PWD@ (_tide_pwd) \"\$$prompt_var[1][1]\")'$prompt_and_frame_color'
+        string repeat -Nm(math max 0, \$dist_btwn_sides-\$_tide_pwd_len) '$tide_prompt_icon_connection'
 
-    # OS icon
-    set -l os_icon
-    switch (uname)
-        case Darwin
-            set os_icon \uF179 # Apple logo
-        case Linux
-            set os_icon \uF17C # Linux penguin
-        case '*'
-            set os_icon \uF109 # Generic computer
+        echo \"\$$prompt_var[1][3]$top_right_frame\"
+    end
+    echo -n \e\[0J\"$bot_left_frame\$$prompt_var[1][2]$color_normal \"
+end
+
+function fish_right_prompt
+    set -e _tide_transient || string unescape \"\$$prompt_var[1][4]$bot_right_frame$color_normal\"
+end"
+    else
+        eval "
+function fish_prompt
+    _tide_status=\$status _tide_pipestatus=\$pipestatus if not set -e _tide_repaint
+        jobs -q && jobs -p | count | read -lx _tide_jobs
+        $fish_path -c \"set _tide_pipestatus \$_tide_pipestatus
+set _tide_parent_dirs \$_tide_parent_dirs
+PATH=\$(string escape \"\$PATH\") CMD_DURATION=\$CMD_DURATION fish_bind_mode=\$fish_bind_mode set $prompt_var (_tide_2_line_prompt)\" &
+        builtin disown
+
+        command kill \$_tide_last_pid 2>/dev/null
+        set -g _tide_last_pid \$last_pid
     end
 
-    # Delimiter
-    set -l delim \u276F
-    string match -qi "*.utf-8" -- $LANG $LC_CTYPE $LC_ALL; or set delim ">"
-    fish_is_root_user; and set delim "#"
+    math \$COLUMNS-(string length -V \"\$$prompt_var[1][1]\$$prompt_var[1][3]\")+$column_offset | read -lx dist_btwn_sides
 
-    # Path
-    set -l cwd (set_color $fish_color_cwd)
+    echo -ns $add_newline'$top_left_frame'(string replace @PWD@ (_tide_pwd) \"\$$prompt_var[1][1]\")'$prompt_and_frame_color'
+    string repeat -Nm(math max 0, \$dist_btwn_sides-\$_tide_pwd_len) '$tide_prompt_icon_connection'
+    echo -ns \"\$$prompt_var[1][3]$top_right_frame\"\n\"$bot_left_frame\$$prompt_var[1][2]$color_normal \"
+end
 
-    # Prompt host
-    if not set -q prompt_host
-        set -g prompt_host ""
-        if set -q SSH_TTY
-            or begin
-                command -sq systemd-detect-virt
-                and systemd-detect-virt -q
+function fish_right_prompt
+    string unescape \"\$$prompt_var[1][4]$bot_right_frame$color_normal\"
+end"
+    end
+else # one line prompt initialization
+    test "$tide_prompt_add_newline_before" = true && set -l add_newline '\0'
+
+    math 5 -$tide_prompt_min_cols | read -l column_offset
+    test $column_offset -ge 0 && set column_offset "+$column_offset"
+
+    if test "$tide_prompt_transient_enabled" = true
+        eval "
+function fish_prompt
+    set -lx _tide_status \$status
+    _tide_pipestatus=\$pipestatus if not set -e _tide_repaint
+        jobs -q && jobs -p | count | read -lx _tide_jobs
+        $fish_path -c \"set _tide_pipestatus \$_tide_pipestatus
+set _tide_parent_dirs \$_tide_parent_dirs
+PATH=\$(string escape \"\$PATH\") CMD_DURATION=\$CMD_DURATION fish_bind_mode=\$fish_bind_mode set $prompt_var (_tide_1_line_prompt)\" &
+        builtin disown
+
+        command kill \$_tide_last_pid 2>/dev/null
+        set -g _tide_last_pid \$last_pid
+    end
+
+    if set -q _tide_transient
+        echo -n \e\[0J
+        add_prefix= _tide_item_character
+        echo -n '$color_normal '
+    else
+        math \$COLUMNS-(string length -V \"\$$prompt_var[1][1]\$$prompt_var[1][2]\")$column_offset | read -lx dist_btwn_sides
+        string replace @PWD@ (_tide_pwd) $add_newline \$$prompt_var[1][1]'$color_normal '
+    end
+end
+
+function fish_right_prompt
+    set -e _tide_transient || string unescape \"\$$prompt_var[1][2]$color_normal\"
+end"
+    else
+        eval "
+function fish_prompt
+    _tide_status=\$status _tide_pipestatus=\$pipestatus if not set -e _tide_repaint
+        jobs -q && jobs -p | count | read -lx _tide_jobs
+        $fish_path -c \"set _tide_pipestatus \$_tide_pipestatus
+set _tide_parent_dirs \$_tide_parent_dirs
+PATH=\$(string escape \"\$PATH\") CMD_DURATION=\$CMD_DURATION fish_bind_mode=\$fish_bind_mode set $prompt_var (_tide_1_line_prompt)\" &
+        builtin disown
+
+        command kill \$_tide_last_pid 2>/dev/null
+        set -g _tide_last_pid \$last_pid
+    end
+
+    math \$COLUMNS-(string length -V \"\$$prompt_var[1][1]\$$prompt_var[1][2]\")$column_offset | read -lx dist_btwn_sides
+    string replace @PWD@ (_tide_pwd) $add_newline \$$prompt_var[1][1]'$color_normal '
+end
+
+function fish_right_prompt
+    string unescape \"\$$prompt_var[1][2]$color_normal\"
+end"
+    end
+end
+
+eval "function _tide_on_fish_exit --on-event fish_exit
+    set -e $prompt_var
+end"
+
+if test "$tide_prompt_transient_enabled" = true
+    function _tide_enter_transient
+        # If the commandline will be executed, or is empty
+        if commandline --is-valid || test -z "$(commandline)"
+            # Pager open usually means selecting, not running
+            # Can be untrue, but it's better than the alternative
+            if not commandline --paging-mode
+                set -g _tide_transient
+                set -g _tide_repaint
+                commandline -f repaint
             end
-            set prompt_host $usercolor$USER$normal@(set_color $fish_color_host)$hostname$normal":"
         end
+        commandline -f execute
     end
 
-    # Prompt pwd
-    set -l pwd (prompt_pwd)
-
-    # Prompt status
-    set -l prompt_status
-    test $last_status -ne 0; and set prompt_status (set_color $fish_color_status)"[$last_status]$normal"
-
-    # Time display for the first line
-    set -l time_display (set_color brgrey)\uF017" "(date "+%I:%M:%S %p")(set_color normal)
-
-    # Build the actual left side content to measure accurately
-    set -l git_info (fish_vcs_prompt '%s' 2>/dev/null)
-    set -l left_content "$os_icon $prompt_host$pwd $git_info "
-
-    # Strip all color codes and measure the actual visible length
-    set -l left_visible (string replace -ra '\e\[[0-9;]*[mK]' '' $left_content)
-    set -l time_visible (string replace -ra '\e\[[0-9;]*[mK]' '' $time_display)
-
-    set -l terminal_width $COLUMNS
-
-    set -l left_length (string length $left_visible)
-    set -l time_length (string length $time_visible)
-
-    # Account for Unicode clock icon taking 2 visual columns instead of 1
-    set time_length (math $time_length + 1)
-
-    # Add a few extra spaces to push time closer to the right edge
-    set -l spaces_needed (math max 0, $terminal_width - $left_length - $time_length + 4)
-    set -l spacing (string repeat -n $spaces_needed " ")
-
-    # OS icon + path + spacing + time (first line)
-    echo -s $os_icon " " $prompt_host $cwd $pwd $normal " " $git_info " " $spacing $time_display
-
-    # Status + delimiter + space (second line)
-    echo -n -s $prompt_status (set_color 00da00) $delim $normal " "
+    bind \r _tide_enter_transient
+    bind \n _tide_enter_transient
+    bind -M insert \r _tide_enter_transient
+    bind -M insert \n _tide_enter_transient
 end
