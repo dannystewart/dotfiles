e3a36fc1a1f652495cfccf7c935c183cfd7cee03
Danny Stewart <danny@stewart.cc>
2025-06-26 18:01:00 -0400

    remove(fish): delete custom mosh function
    This removes the mosh function that was used to specify a different mosh-server path for specific hosts.

--- dot_config/private_fish/functions/mosh.fish
+++
@@ -1,9 +0,0 @@
-function mosh --description 'Enhanced mosh function'
-    switch "$argv[1]"
-        case saber calufrax defiant
-            set -e argv[1] # Remove first argument
-            command mosh --server=/opt/homebrew/bin/mosh-server $argv
-        case '*'
-            command mosh $argv
-    end
-end
