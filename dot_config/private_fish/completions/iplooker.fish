62a63dd13d23287bef265758653ac03ea4481268
Danny Stewart <danny@stewart.cc>
2025-06-30 15:40:38 -0400

    feat(fish): add completions for iplooker command
    Adds Fish shell completions for the iplooker command with the following options:
    - Default completion for IP address lookup
    - `-m/--me` flag to get external IP address
    - `-l/--lookup` flag to get lookup for current IP address

---
+++ dot_config/private_fish/completions/iplooker.fish
@@ -0,0 +1,4 @@
+# Auto-generated completions for iplooker
+complete -c iplooker -d "the IP address to look up" -f
+complete -c iplooker -s m -l me -d "get your external IP address" -f
+complete -c iplooker -s l -l lookup -d "get lookup for your IP address" -f
