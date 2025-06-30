72b94aab2bea8d8d3b2c8f7c25ed02ce0e56ce09
Danny Stewart <danny@stewart.cc>
2025-06-30 15:33:57 -0400

    feat(fish): set --group-directories-first by default in eza
    Removes redundant ls abbreviations that use --group-directories-first flag by moving this configuration directly into the ls function wrapper.
    
    This simplifies the abbreviation list while maintaining the same functionality, as directories will now always be listed first when using eza.

--- dot_config/private_fish/conf.d/abbreviations.fish
+++ dot_config/private_fish/conf.d/abbreviations.fish
@@ -26,13 +26,10 @@ abbr -a ca chezmoi apply
 # ls abbreviations
 abbr -a l ls -1
 abbr -a ll ls -l --no-user
-abbr -a lf ls -1 --group-directories-first # directories first
 abbr -a lt ls --tree --level=1 # tree view
 abbr -a la ls -la --no-user # same as above, just with hidden files
-abbr -a laf ls -la --no-user --group-directories-first # directories first
 abbr -a lz ls -l --no-user --total-size -rs size # with folder sizes, sorted by size
 abbr -a lv ls -lga --git # verbose (includes user, group, and git status)
-abbr -a lvf ls -lga --git --group-directories-first # verbose with directories first
 abbr -a lvz ls -lga --git --total-size -rs size # with folder sizes, sorted by size
 
 # Other shell helpers
--- dot_config/private_fish/functions/ls.fish
+++ dot_config/private_fish/functions/ls.fish
@@ -1,6 +1,6 @@
 function ls --description="ls replacement wrapper using exa with smart fallback"
     if command -v eza &>/dev/null
-        command eza --no-quotes --time-style='+%Y.%m.%d %I:%M %p' --icons $argv
+        command eza --no-quotes --time-style='+%Y.%m.%d %I:%M %p' --group-directories-first --icons $argv
     else
         command ls --color=auto $argv
     end
