57689200b06a91ba82fc380af1ef5fe5847b9095
Danny Stewart <danny@stewart.cc>
2025-07-29 08:12:16 -0400

    feat(rmknown): enhance with icons in output messages
    Adds visual icons to the output messages in the rmknown fish function:
    - Success message now includes a checkmark icon
    - Error messages now include an exclamation mark icon
    - Adds exclamation marks to all messages for better emphasis

--- dot_config/private_fish/functions/rmknown.fish
+++ dot_config/private_fish/functions/rmknown.fish
@@ -2,11 +2,11 @@ function rmknown --description 'Remove line from ~/.ssh/known_hosts'
     if test -n "$argv[1]"
         if string match -qr '^[0-9]+$' "$argv[1]"
             awk -v line="$argv[1]" 'NR != line' ~/.ssh/known_hosts > ~/.ssh/known_hosts.tmp && mv ~/.ssh/known_hosts.tmp ~/.ssh/known_hosts
-            success "Removed line $argv[1]"
+            success "󰄬 Removed line $argv[1]!"
         else
-            error "Error: Not a number" >&2
+            error " Error: Not a number!" >&2
         end
     else
-        error "Error: No line number specified" >&2
+        error " Error: No line number specified!" >&2
     end
 end
