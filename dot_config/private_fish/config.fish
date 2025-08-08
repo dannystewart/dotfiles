fadb87054fc87d70b0ef240d03d372dc2efb1609
Danny Stewart <danny@stewart.cc>
2025-08-07 22:20:25 -0400

    feat(fish): add nvm support to fish shell
    Adds Node Version Manager (nvm) functionality to the fish shell configuration:
    
    - Implements core nvm command with install, use, list, and uninstall subcommands
    - Adds fish shell completions for nvm commands
    - Creates helper functions for version management, activation/deactivation
    - Sets up automatic version detection from .nvmrc files
    - Configures NVM_DIR environment variable in config.fish
    
    This implementation allows for managing multiple Node.js versions through fish shell.

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -7,6 +7,7 @@ set -gx EZA_ICONS_AUTO true
 set -gx HOMEBREW_NO_ENV_HINTS true
 set -gx LESSHISTFILE /dev/null
 set -gx MANPAGER "sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -p -lman'"
+set -gx NVM_DIR "$HOME/.nvm"
 set -gx TZ America/New_York
 
 # Interactive shell configuration
