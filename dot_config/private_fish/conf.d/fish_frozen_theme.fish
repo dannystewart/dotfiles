60f233d5a687e12afef29eeb24047f107c235945
Danny Stewart <danny@stewart.cc>
2026-01-06 14:12:18 -0500

    refactor(fish): add frozen variables and keybindings from 4.3 upgrade

---
+++ dot_config/private_fish/conf.d/fish_frozen_theme.fish
@@ -0,0 +1,57 @@
+# This file was created by fish when upgrading to version 4.3, to migrate
+# theme variables from universal to global scope.
+# Don't edit this file, as it will be written by the web-config tool (`fish_config`).
+# To customize your theme, delete this file and see
+#     help interactive#syntax-highlighting
+# or
+#     man fish-interactive | less +/^SYNTAX.HIGHLIGHTING
+# for appropriate commands to add to ~/.config/fish/config.fish instead.
+# See also the release notes for fish 4.3.0 (run `help relnotes`).
+
+set --global fish_color_autosuggestion '#565f89'
+set --global fish_color_cancel normal
+set --global fish_color_command green --bold
+set --global fish_color_comment '#565f89'
+set --global fish_color_cwd '#008AB3'
+set --global fish_color_cwd_root red
+set --global fish_color_end '#ff9e64'
+set --global fish_color_error red
+set --global fish_color_escape '#89ddff'
+set --global fish_color_hg_added green
+set --global fish_color_hg_clean green
+set --global fish_color_hg_copied magenta
+set --global fish_color_hg_deleted red
+set --global fish_color_hg_dirty red
+set --global fish_color_hg_modified yellow
+set --global fish_color_hg_renamed magenta
+set --global fish_color_hg_unmerged red
+set --global fish_color_hg_untracked yellow
+set --global fish_color_history_current --bold
+set --global fish_color_host normal
+set --global fish_color_host_remote yellow
+set --global fish_color_keyword '#7aa2f7'
+set --global fish_color_match '#f7768e'
+set --global fish_color_normal normal
+set --global fish_color_operator '#ff9e64'
+set --global fish_color_option '#8a92b8'
+set --global fish_color_param '#a9b1d6'
+set --global fish_color_quote '#9ece6a'
+set --global fish_color_redirection '#2ac3de'
+set --global fish_color_search_match '--background=#0088cc'
+set --global fish_color_selection '--background=#0088cc'
+set --global fish_color_status normal
+set --global fish_color_user normal
+set --global fish_color_valid_path --underline
+set --global fish_pager_color_background normal
+set --global fish_pager_color_completion '#a9b1d6'
+set --global fish_pager_color_description '#787c99'
+set --global fish_pager_color_prefix '#89ddff'
+set --global fish_pager_color_progress '#ffffff' '--background=#414868'
+set --global fish_pager_color_secondary_background normal
+set --global fish_pager_color_secondary_completion '#a9b1d6'
+set --global fish_pager_color_secondary_description '#787c99'
+set --global fish_pager_color_secondary_prefix '#89ddff'
+set --global fish_pager_color_selected_background '--background=#0066aa'
+set --global fish_pager_color_selected_completion '#ffffff'
+set --global fish_pager_color_selected_description '#ffffff'
+set --global fish_pager_color_selected_prefix '#89ddff'
